7fbe06a2119340b9e7ea10085c73ca7a76d6cc1d
Jin hyeon <kangjin627@gmail.com>
2024-07-17 08:51:10 +0900

    ADD :: Something

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { MyPage } from "./pages/My";
+import { MapPage } from "./pages/Map";
 
 function App() {
   return (
     <Routes>
-      <Route path='/login' />
+      <Route path="/login" />
+      <Route path="/my" element={<MyPage />} />
+      <Route path="/map" element={<MapPage />} />
     </Routes>
   );
 }
