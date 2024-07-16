15dbdf908dd2dac2afe736406902ba9aa959c1a8
Jin hyeon <kangjin627@gmail.com>
2024-07-16 15:30:29 +0900

    ADD :: Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      123
-    </div>
+    <Routes>
+      <Route path='/login' />
+    </Routes>
   );
 }
 
